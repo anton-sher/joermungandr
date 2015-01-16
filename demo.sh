bff9a6028dba97bfec95b90bd72e9b1e68d2cb13
Anton Sherstiuk <anton.sherstiuk@smaato.com>
2015-01-16 15:24:20 +0100

    echoes in demo.sh

--- demo.sh
+++ demo.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+echo "1. Python."
 rm joermungandr.rb
 python3 joermungandr-init.py
 
+echo "2. Ruby."
 rm joermungandr.groovy
 ruby joermungandr.rb
 
+echo "3. Groovy."
 rm joermungandr.sh
 groovy joermungandr.groovy
 
+echo "4. Bash."
 rm joermungandr.py
 bash joermungandr.sh
 
+echo "Checking..."
 diff -q joermungandr.py joermungandr-init.py
 if [ $? == 0 ]; then
     echo "Joermungandr!"
