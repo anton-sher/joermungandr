8fe265fb76b54ebcd26233a655ca5caf7c58643d
Anton Sherstiuk <anton.sherstiuk@smaato.com>
2015-01-16 12:20:37 +0100

    output ruby code that prints python code to stdout. gitignore.

--- quine-init.py
+++ quine-init.py
@@ -1,15 +1,31 @@
 magic = r"""
-def q():
-    return "magic = r\"\"\"" + magic + "\"\"\"\n\n" + magic
-
-
-with open("quine.py", mode='w') as f:
-    print(q(), file=f)"""
+def quote(s):
+    return s.replace('\\', '\\\\').replace('"', '\\"').replace('\n', '\\n')
 
 
 def q():
     return "magic = r\"\"\"" + magic + "\"\"\"\n\n" + magic
 
 
-with open("quine.py", mode='w') as f:
-    print(q(), file=f)
+def ruby():
+    with open("quine.rb", mode='w') as f:
+        print("print \"" + quote(q()) + "\"", file=f)
+
+
+ruby()"""
+
+
+def quote(s):
+    return s.replace('\\', '\\\\').replace('"', '\\"').replace('\n', '\\n')
+
+
+def q():
+    return "magic = r\"\"\"" + magic + "\"\"\"\n\n" + magic
+
+
+def ruby():
+    with open("quine.rb", mode='w') as f:
+        print("print \"" + quote(q()) + "\"", file=f)
+
+
+ruby()
